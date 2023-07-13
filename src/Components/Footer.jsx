716139700042fc5ec0a384d3059927360f284c91
Erick384 <erickwachuma@gmail.com>
2023-07-13 20:48:42 +0300

    Uploading Commits

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">😎 Erick Wachuma</a>.
+    </div>
+  );
+};
---
+++ src/Components/Pics.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const Pics = () => {
+  return (
+    <div className="bgimage2">
+      <div className="img1">
+        <img
+          src="./images/image-product-1-thumbnail.jpg"
+          height="100px"
+          width="100px"
+        />
+      </div>
+      <div className="img1">
+        <img
+          src="./images/image-product-2-thumbnail.jpg"
+          height="100px"
+          width="100px"
+        />
+      </div>
+      <div className="img1">
+        <img
+          src="./images/image-product-3-thumbnail.jpg"
+          height="100px"
+          width="100px"
+        />
+      </div>
+      <div className="img1">
+        <img
+          src="./images/image-product-4-thumbnail.jpg"
+          height="100px"
+          width="100px"
+        />
+      </div>
+    </div>
+  );
+};
